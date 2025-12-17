deba0a10ba7ae2d3450ecc07699f20b3a71bbdf6
Vkcode95 <139173564+Vkcode95@users.noreply.github.com>
2025-12-17 21:17:20 +0530

    Update onboarding_2

--- lid/modules/auth/onboarding_2.dart
+++ lid/modules/auth/onboarding_2.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
+import '../../routes/app_routes.dart';
 
+class Onboarding2 extends StatelessWidget {
+  const Onboarding2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Sell & Grow', style: TextStyle(fontSize: 26)),
+          const SizedBox(height: 24),
+          ElevatedButton(
+            onPressed: () => Navigator.pushNamed(context, AppRoutes.onboarding3),
+            child: const Text('Next'),
+          ),
+        ],
+      ),
+    );
+  }
+}
