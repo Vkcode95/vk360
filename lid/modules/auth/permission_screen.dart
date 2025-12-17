9afd7bf8cf93515c65883f2e9914d1713a338ada
Vkcode95 <139173564+Vkcode95@users.noreply.github.com>
2025-12-17 21:21:19 +0530

    Update permission_screen

--- lid/modules/auth/permission_screen.dart
+++ lid/modules/auth/permission_screen.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
+import '../../routes/app_routes.dart';
 
+class PermissionsScreen extends StatelessWidget {
+  const PermissionsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Permissions')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushReplacementNamed(context, AppRoutes.home);
+          },
+          child: const Text('Allow & Continue'),
+        ),
+      ),
+    );
+  }
+}
