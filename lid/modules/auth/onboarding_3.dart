a590297f37638818a5406be9d193c1998c9049b9
Vkcode95 <139173564+Vkcode95@users.noreply.github.com>
2025-12-17 21:18:08 +0530

    Update onboarding_3

--- lid/modules/auth/onboarding_3.dart
+++ lid/modules/auth/onboarding_3.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
+import '../../routes/app_routes.dart';
 
+class Onboarding3 extends StatelessWidget {
+  const Onboarding3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Payments & Delivery', style: TextStyle(fontSize: 26)),
+          const SizedBox(height: 24),
+          ElevatedButton(
+            onPressed: () => Navigator.pushReplacementNamed(context, AppRoutes.login),
+            child: const Text('Get Started'),
+          ),
+        ],
+      ),
+    );
+  }
+}
