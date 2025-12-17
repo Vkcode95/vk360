b51103ebdc3b6a32cb7e05e7254d3216fcc368b0
Vkcode95 <139173564+Vkcode95@users.noreply.github.com>
2025-12-17 21:06:59 +0530

    Add global theme

--- lib/core/theme.dart
+++ lib/core/theme.dart
@@ -1,1 +1,11 @@
+import 'package:flutter/material.dart';
 
+class VK360Theme {
+  static ThemeData get light {
+    return ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: Colors.indigo,
+      appBarTheme: const AppBarTheme(centerTitle: true),
+    );
+  }
+}
