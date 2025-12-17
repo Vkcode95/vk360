6346510236b085bbc6677bbb6cff80148fe62b3b
Vkcode95 <139173564+Vkcode95@users.noreply.github.com>
2025-12-17 21:03:03 +0530

    Configure app wrapper and routing entry

--- lib/app.dart
+++ lib/app.dart
@@ -1,1 +1,19 @@
 
+import 'package:flutter/material.dart';
+import 'routes/app_routes.dart';
+import 'core/theme.dart';
+
+class VK360App extends StatelessWidget {
+  const VK360App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'VK_360',
+      debugShowCheckedModeBanner: false,
+      theme: VK360Theme.light,
+      initialRoute: AppRoutes.splash,
+      routes: AppRoutes.routes,
+    );
+  }
+}
