bf3d3a6dd14fa4d783441a4fbbab9bf87c5a9aeb
Vkcode95 <139173564+Vkcode95@users.noreply.github.com>
2025-12-17 21:20:29 +0530

    Update register_screen

--- lid/modules/auth/register_screen.dart
+++ lid/modules/auth/register_screen.dart
@@ -1,1 +1,33 @@
+import 'package:flutter/material.dart';
+import '../../routes/app_routes.dart';
 
+class RegisterScreen extends StatelessWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final nameCtrl = TextEditingController();
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Complete Profile')),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(
+              controller: nameCtrl,
+              decoration: const InputDecoration(labelText: 'Full Name'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, AppRoutes.permissions);
+              },
+              child: const Text('Continue'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
